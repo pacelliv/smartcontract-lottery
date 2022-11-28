c43da9ce069386838ceb21d3a1e4802bbd78b396
Eugenio Pacelli Flores <flores.eugenio03@gmail.com>
2022-11-28 08:14:43 -0500

    modified comments

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -59,20 +59,18 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                       "RaffleEnter"
                   )
               })
+
+              // For this hook we want RaffleState to be in a close state, currently the state is
+              // OPEN to change it from OPEN to CALCULATING we need to call performUpkeep
+              // but performUpkeep can only be call if checkUpkeep returns TRUE -- otherwise
+              // it will revert with "Raffle__UpkeepNotNeeded()" -- to make this function return
+              // TRUE we need isOpen, timePassed, hasPlayers and hasBalance TRUE.
               it("doesn't allow entrance when raffle is calculating", async () => {
                   await raffle.enterRaffle({ value: entranceFee })
-                  // We want RaffleState to be in a close state, currently the state is OPEN
-                  // to change it from OPEN to CALCULATING me we need to call performUpkeep()
-                  // but performUpkeep() can only be call if checkUpkeep returns TRUE otherwise
-                  // it will revert with Raffle__UpkeepNotNeeded() to make this function to return
-                  // TRUE we need to set make isOpen, timePassed, hasPlayers and hasBalance TRUE.
-                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
-                  // We pass an empty array to only mine one block.
-                  await network.provider.send("evm_mine", [])
-                  // Pretend to be the Chainlink Keeper, call performUpkeep() and pass it an empty
-                  // array, the empty array represents the empty callData.
-                  await raffle.performUpkeep([])
-                  // Now RaffleState should be in an OPEN state.
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]) // To accelerate the time we use this method to make timePassed true
+                  await network.provider.send("evm_mine", []) // We pass an empty array to only mine one block.
+                  await raffle.performUpkeep([]) // Pretend to be the a Chainlink Node by calling performUpkeep() and pass to it an empty array that represents the empty callData
+                  // Now RaffleState should be in a CALCULATING state.
                   await expect(raffle.enterRaffle({ value: entranceFee })).to.be.revertedWith(
                       "Raffle__NotOpen"
                   )
@@ -80,13 +78,12 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
           })
 
           describe("checkUpkeep", () => {
-              it("returns false if people haven't sent any ETH", async () => {
+              // Since checkUpkeep is a public function using "await raffle.checkUpkeep([])" will result in
+              // a transaction, but I only want to simulate the transaction to see if the state of upkeepNeeded is false
+              it("returns false if there's no balance in the Raffle", async () => {
                   await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
                   await network.provider.send("evm_mine", [])
-                  // Since checkUpkeep is a public function using await raffle.checkUpkeep([]) will
-                  // result in sending a transaction, I only want to simulate the transaction to see
-                  // so we use callstatic to simulate the transaction.
-                  await raffle.callStatic.checkUpkeep([])
+                  await raffle.callStatic.checkUpkeep([]) // Use callstatic to simulate the transaction.
                   const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([])
                   assert(!upkeepNeeded)
               })
@@ -111,24 +108,22 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   assert(tx)
               })
               it("reverts when checUpkeep is false", async () => {
-                  // I can be more specific by passing the parameters in Raffle__UpKeepNotNeeded()
+                  // If needed we can be more specific by passing the parameters to Raffle__UpKeepNotNeeded()
                   await expect(raffle.performUpkeep([])).to.be.revertedWith(
                       "Raffle__UpKeepNotNeeded"
                   )
               })
-              it("updates the raffle state, emits and event and calls the vrfCoordinator", async () => {
+              it("updates the raffle state, emits RequestedRaffleWinner and calls the vrfCoordinator", async () => {
                   await raffle.enterRaffle({ value: entranceFee })
                   await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
                   await network.provider.send("evm_mine", [])
                   const txResponse = await raffle.performUpkeep([])
-                  const txReceipt = await txResponse.wait(1)
-                  // From my contract I can get requestId from emit RequestedRaffleWinner(requestId)
-                  // However if look inside my VRFCoordinatorV2Mock.sol, I will see that If I
-                  // call requestRandomWords() from this mock it also emits an event with randomWords requested
-                  // This means that the emit event in my contract is redundant line 152. But for educational
-                  // purposes I will leave it there.
-                  // The event in the zeroed index is uint256 requestId = i_vrfCoordinator.requestRandomWords(),
-                  // that's why I need the event in the index 1.
+                  const txReceipt = await txResponse.wait(1) // To emit RequestedRaffleWinner I need to send a transaction
+                  // If look inside VRFCoordinatorV2Mock.sol, I will see that If I call requestRandomWords() from this mock
+                  // it also emits an event with randomWords requested -- this means that the emitted event in my contract is
+                  // redundant (line 152), but for educational purposes I will leave it there.
+                  // The event in the zeroed index is "uint256 requestId = i_vrfCoordinator.requestRandomWords()",
+                  // that's why we need the event in the index 1.
                   const requestId = txReceipt.events[1].args.requestId
                   const raffleState = await raffle.getRaffleState()
                   assert(requestId.toNumber() > 0)
@@ -137,13 +132,12 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
           })
 
           describe("fulfillRandomWords", () => {
+              // fulfillRandomWords can only be call if there's a requestId, that's guaranteed in the beforeEach hook
               beforeEach(async () => {
                   await raffle.enterRaffle({ value: entranceFee })
                   await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
                   await network.provider.send("evm_mine", [])
               })
-              // fulfillRandomWords can olny be call if there's a requestId
-              // if you check inside VRFCoordinatorV2Mock you will see the fulfillRandomWords() function
               it("can only be call after perfomUpkeep", async () => {
                   await expect(
                       vrfCoordinatorV2Mock.fulfillRandomWords(0, raffle.address)
@@ -152,7 +146,21 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                       vrfCoordinatorV2Mock.fulfillRandomWords(1, raffle.address)
                   ).to.be.revertedWith("nonexistent request")
               })
-              it("picks a winner, resets the lottery and sends money", async () => {
+
+              // 1. First we want to call performUpkeep (mock being Chainlink Keepers)
+              // 2. Then we want to call fulfillRandomWords (mock being Chainlink VRF)
+              // 3. After both these things have happened we can record if:
+              //    - Recent winner gets recorded
+              //    - RaffleState, s_players and s_lastTimeStamp were reset
+              // 4. To verify the above in a tesnet we will have to wait for fulfillRandoWords
+              // to be called, but in a local blockchain there's no need for that because
+              // we can adjust our blockchain to do whatever we want, but for this test we will
+              // simulate that we have to wait for that event to be called as If we were in a tesnet.
+              // 5. To achieve that we will need to set up an event listener.
+              // 6. Basically we want the test to not finish until the listener has stopped listening
+              // so we need to create a new Promise.
+              it("picks a winner, resets the lottery and sends the prize", async () => {
+                  console.log("Setting up test...")
                   const additionalEntrances = 3
                   const startingIndex = 2 // deployer = 0
                   const accounts = await ethers.getSigners()
@@ -161,25 +169,13 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                       await accountConnectedRaffle.enterRaffle({ value: entranceFee })
                   }
                   const startingTimeStamp = await raffle.getLatestTimeStamp()
-                  // 1. First we want to call performUpkeep (mock being Chainlink Keepers)
-                  // 2. Then we want to call fulfillRandomWords (mock being Chainlink VRF)
-                  // 3. After both these things have happened we can record if:
-                  //    - Recent winner get recorded?
-                  //    - RaffleState, s_players and s_lastTimeStamp were reset
-                  // 4. To verify the above in a tesnet we will have to wait for fulfillRandoWords
-                  // to be called, but in a local enviroment there's no need for that because
-                  // we can adjust our blockchain to do whatever we want. BUT for this test we will
-                  // simulate that we have to wait for that event to be called as If we were in a tesnet.
-                  // 5. To achieve that we will need to set up an event listener.
-                  // 6. Basically we want the test to not finish until the listener has stopped listening
-                  // so we need to create a new Promise.
                   await new Promise(async (resolve, reject) => {
                       raffle.once("winnerPicked", async () => {
                           // setting up the listener
                           console.log("winnerPicked event fired!")
                           try {
                               const recentWinner = await raffle.getRecentWinner()
-                              console.log(`The winner is ${recentWinner}`)
+                              console.log(`The winner is ${recentWinner}`) // In our local blockchain there's no randomness, the first player to enter the Raffle will be the winner.
                               console.log(accounts[0].address)
                               console.log(accounts[1].address)
                               console.log(accounts[2].address)
@@ -206,15 +202,17 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                               reject(e)
                           }
                       })
+
+                      console.log("Entering Raffle...") // enter raffle
                       const tx = await raffle.performUpkeep([])
                       const txReceipt = await tx.wait(1)
+                      console.log("Time to wait...")
                       const winnerStartingBalance = await accounts[2].getBalance()
-                      // check inside VRFCoordinatorV2Mock to find fulfillRandomWords(params)
+                      // check inside VRFCoordinatorV2Mock to find fulfillRandomWords parameters
                       await vrfCoordinatorV2Mock.fulfillRandomWords(
                           txReceipt.events[1].args.requestId,
                           raffle.address
                       )
-
                       // this code won't complete until the listener finishes listening
                   })
               })
