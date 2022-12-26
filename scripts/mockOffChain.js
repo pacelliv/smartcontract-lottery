f2a45cb8f50aec7f59f4023682d2b1d68ce6003f
Eugenio Pacelli Flores <flores.eugenio03@gmail.com>
2022-12-25 21:37:43 -0500

    completed mocking script

---
+++ scripts/mockOffChain.js
@@ -0,0 +1,33 @@
+const { ethers, network } = require("hardhat")
+
+async function mockKeepers() {
+    const raffle = await ethers.getContract("Raffle")
+    const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""))
+    const { upkeepNeeded } = await raffle.callStatic.checkUpkeep(checkData)
+    console.log(upkeepNeeded)
+    if (upkeepNeeded) {
+        const tx = await raffle.performUpkeep(checkData)
+        const txReceipt = await tx.wait(1)
+        const requestId = txReceipt.events[1].args.requestId
+        console.log(`Performed upkeep with RequestId: ${requestId}`)
+        if (network.config.chainId === 31337) {
+            await mockVrf(requestId, raffle)
+        }
+    }
+}
+
+async function mockVrf(requestId, raffle) {
+    console.log("We're on a local network so we'll pretend to be the VRF Service...")
+    const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+    await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, raffle.address)
+    console.log("Winner picked!")
+    const recentWinner = await raffle.getRecentWinner()
+    console.log(`The recent winner is ${recentWinner}`)
+}
+
+mockKeepers()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
